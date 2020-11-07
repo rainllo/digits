dd6389595dfdb36a34c07588f4348f0e78a1d85b
rainllo <58460379+rainllo@users.noreply.github.com>
2020-11-06 21:47:00 -1000

    contact forms finished in 26 minutes.

--- app/imports/ui/components/Contact.jsx
+++ app/imports/ui/components/Contact.jsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import { Card, Image } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class Contact extends React.Component {
   render() {
     return (
-          <Card centered>
-            <Card.Content>
-              <Image
-                  floated='right'
-                  size='mini'
-                  src={this.props.contact.image}
-              />
-              <Card.Header>{this.props.contact.firstName} {this.props.contact.lastName}</Card.Header>
-              <Card.Meta>{this.props.contact.address}</Card.Meta>
-              <Card.Description>
-                {this.props.contact.description}
-              </Card.Description>
-            </Card.Content>
-          </Card>
+        <Card centered>
+          <Card.Content>
+            <Image
+                floated='right'
+                size='mini'
+                src={this.props.contact.image}
+            />
+            <Card.Header>{this.props.contact.firstName} {this.props.contact.lastName}</Card.Header>
+            <Card.Meta>{this.props.contact.address}</Card.Meta>
+            <Card.Description>
+              {this.props.contact.description}
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <Link to={`/edit/${this.props.contact._id}`}>Edit</Link>
+          </Card.Content>
+        </Card>
     );
   }
 }
